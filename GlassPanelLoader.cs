4383c3d4997bf5bca107ea178cadbfc188af32d4
SibusisoMadlala <87149996+SibusisoMadlala@users.noreply.github.com>
2025-06-17 10:37:20 +0200

    Update GlassPanelLoader.cs

--- GlassPanelLoader.cs
+++ GlassPanelLoader.cs
@@ -12,11 +12,22 @@ namespace Optimizer
         {
             return new List<GlassPanel>
             {
-                new GlassPanel { Id = 1, Width = 1000, Height = 500 },
-                new GlassPanel { Id = 2, Width = 800, Height = 800 },
-                new GlassPanel { Id = 3, Width = 600, Height = 1200 },
-                new GlassPanel { Id = 4, Width = 500, Height = 500 },
-                new GlassPanel { Id = 5, Width = 1500, Height = 900 }
+                new GlassPanel { Id = 1, Width = 700, Height = 484 },
+                new GlassPanel { Id = 2, Width = 501, Height = 249 },
+                new GlassPanel { Id = 3, Width = 1132, Height = 675 },
+                new GlassPanel { Id = 4, Width = 485, Height = 433 },
+                new GlassPanel { Id = 5, Width = 522, Height = 466 },
+                      
+                new GlassPanel { Id = 7, Width = 1726, Height = 926},
+                new GlassPanel { Id = 6, Width = 700, Height = 484 },
+                new GlassPanel { Id = 8, Width = 501, Height = 249 },
+                new GlassPanel { Id = 9, Width = 1132, Height = 675 },
+                
+                
+                new GlassPanel { Id = 10, Width = 1726, Height = 926},
+                new GlassPanel { Id = 11, Width = 700, Height = 484 },
+                new GlassPanel { Id = 12, Width = 501, Height = 249 },
+                new GlassPanel { Id = 13, Width = 1132, Height = 675 }
             };
         }
     }
